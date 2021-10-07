12836e15af64d94b11eb0fc95c8b770e05eff495
Androsova Elizaveta <androsovaelizaveta@mail.ru>
2021-10-07 15:11:25 +0300

    Code after the first review

--- homework.py
+++ homework.py
@@ -1,25 +1,30 @@
 """Calories / cash calculator.
 
-This module provides calories/cash statistics for today and last 7 days,
-allows to set a daily limit and check if it is reached (how much is left -
-if not).
+This module provides calories/cash stats for today and last 7 days, allows
+to set a daily limit and check if it is reached (how much is left - if not).
 """
+from __future__ import annotations
+from typing import Optional
 import datetime as dt
 
-FORMAT = '%d.%m.%Y'
+FORMAT: str = '%d.%m.%Y'
 
 
 class Record:
     """Record object of input data for calculator.
 
     Attributes:
-        amount: A numeric count of the spent cash/consumed calories in a day.
-        comment: A string explanatory comment for spending/consuming.
-        [date]: A string/datetime.date date for spending/consuming. Default
-                value: None.
+        amount: A count of the spent cash/consumed calories in a day.
+        comment: An explanatory comment for spending/consuming.
+        [date]: A date for spending/consuming. Default value: None.
     """
 
-    def __init__(self, amount, comment, date=None):
+    amount: float
+    comment: str
+    date: dt.date
+
+    def __init__(self, amount: float, comment: str,
+                 date: Optional[str]=None) -> None:
         """Inits Record class and converts date to datetime.date format.
 
         If date is not mentioned it is assigned today date.
@@ -36,60 +41,38 @@ class Calculator:
     """This class contains info about the set daily limit for calories/cash.
 
     Attributes:
-        limit: A numeric daily limit for cash/calories.
+        limit: A daily limit for cash/calories.
     """
 
-    def __init__(self, limit):
+    WEEK_DELTA: dt.date = dt.date.today() - dt.timedelta(days=7)
+    limit: float
+
+    def __init__(self, limit: float) -> None:
         """Inits Calculator class and list of input records."""
         self.limit = limit
-        self.records = []
+        self.records: list[Record] = []
 
-    def add_record(self, record):
+    def add_record(self, record: Record) -> None:
         """Adds Record object to the list of records.
 
         Args:
-            record: A Record object to add in the list of records
-
-        No Returns.
-
-        No Raises.
+            record: An object to add in the list of records.
         """
         self.records.append(record)
 
-    def get_today_stats(self):
-        """Returns calories/cash sum for today.
+    def get_today_stats(self) -> float:
+        """Returns calories/cash sum for today."""
+        return sum(rec.amount for rec in self.records
+                   if rec.date == dt.date.today())
 
-        No Args.
+    def get_week_stats(self) -> float:
+        """Returns calories/cash sum for last 7 days."""
+        return sum(rec.amount for rec in self.records
+                   if dt.date.today() >= rec.date > self.WEEK_DELTA)
 
-        Returns:
-            A numeric count of calories/cash for today.
-
-        No Raises.
-        """
-        day_amounts = 0
-        for rec in self.records:
-            if rec.date == dt.date.today():
-                day_amounts += rec.amount
-        return day_amounts
-
-    def get_week_stats(self):
-        """Returns calories/cash sum for last 7 days.
-
-        No Args.
-
-        Returns:
-            A numeric count of calories/cash for last 7 days.
-
-        No Raises.
-        """
-        week_amounts = 0
-        # This comments are here due to the requirements of pytest.
-        # List of timelta from days=0 to days=7 where day=7 is not included.
-        week_delta = [dt.timedelta(days=day) for day in range(7)]
-        for rec in self.records:
-            if dt.date.today() - rec.date in week_delta:
-                week_amounts += rec.amount
-        return week_amounts
+    def get_remained(self) -> float:
+        '''Returns remained value due to the daily limit.'''
+        return self.limit - self.get_today_stats()
 
 
 class CaloriesCalculator(Calculator):
@@ -97,28 +80,23 @@ class CaloriesCalculator(Calculator):
     due to the daily limit and giving the text information about it.
 
     Attributes:
-        limit: A numeric daily calories limit.
+        limit: A daily calories limit.
     """
 
-    def get_calories_remained(self):
+    def get_calories_remained(self) -> str:
         """Checks if calories limit is reached and returns advice with
         remained calories for today (if limit is not reached).
 
-        No Args.
-
         Returns:
-            A string containing remained calories for today due to the daily
-            limit (if not reached) and advice.
-
-        No Raises.
+            Remained calories for today due to the daily limit (if not
+            reached) and advice.
         """
-        remained = self.limit - super().get_today_stats()
+        remained: float = super().get_remained()
         if remained > 0:
             return ('Сегодня можно съесть что-нибудь ещё, но '
                     'с общей калорийностью не более '
-                    f'{remained} кКал')
-        else:
-            return 'Хватит есть!'
+                    f'{super().get_remained()} кКал')
+        return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
@@ -126,37 +104,36 @@ class CashCalculator(Calculator):
     to the daily limit and giving the text information about it.
 
     Attributes:
-        limit: A numeric daily cash limit.
+        limit: A daily cash limit.
     """
 
-    USD_RATE = 60.
-    EURO_RATE = 70.
-    RUB_RATE = 1.
-    CURRENCIES = {'rub': ['руб', RUB_RATE],
-                  'usd': ['USD', USD_RATE],
-                  'eur': ['EUR', EURO_RATE], }
+    USD_RATE: float = 60.
+    EURO_RATE: float = 70.
+    RUB_RATE: float = 1.
+    CURRENCIES: dict[str, tuple[str, float]] = {'rub': ('руб', RUB_RATE),
+                                                'usd': ('USD', USD_RATE),
+                                                'eur': ('EUR', EURO_RATE), }
 
-    def get_today_cash_remained(self, currency):
+    def get_today_cash_remained(self, currency: str) -> str:
         """Returns the amount of the cash balance from the limit.
 
         Args:
-            currency: A string preference for output cash balance - 'rub' /
-                      'usd' / 'eur'.
+            currency: A preference for output cash balance: 'rub'/'usd'/'eur'.
 
         Returns:
             A string containing remained cash balance (if not 0) for today
             due to the daily limit and advice.
 
-        No Raises.
+        Raises:
+            KeyError: If currency not found in the CURRENCIES dictionary.
         """
-        remained = ((self.limit - super().get_today_stats())
-                    / self.CURRENCIES[currency][1])
-        selected_currency = self.CURRENCIES[currency][0]
+        if super().get_remained() == 0:
+            return 'Денег нет, держись'
+        assert(currency in self.CURRENCIES), 'Нет данных по валюте'
+        currency_name, currency_rate = self.CURRENCIES[currency]
+        remained = (super().get_remained() / currency_rate)
         if remained > 0:
             return (f'На сегодня осталось {remained:.2f} '
-                    f'{selected_currency}')
-        elif remained < 0:
-            return ('Денег нет, держись: твой долг - '
-                    f'{abs(remained):.2f} {selected_currency}')
-        else:
-            return 'Денег нет, держись'
+                    f'{currency_name}')
+        return ('Денег нет, держись: твой долг - '
+                    f'{abs(remained):.2f} {currency_name}')
